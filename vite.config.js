efe6c0ab53c3752ee134ca95462b3c6a379161c0
startour2224 <startour2224@gmail.com>
2026-03-28 00:01:38 +0900

    fix: white screen issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/photo-map/', // ← ここにリポジトリ名を入れる！
+  base: './', // ★この行を必ず追加してください！
 })

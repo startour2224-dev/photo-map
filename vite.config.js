b71b6d57b976ed4c56f47f697c7ce168fb579d04
startour2224 <startour2224@gmail.com>
2026-03-28 00:37:44 +0900

    fix: absolute pathing for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // これがGitHub Pagesでの「パスのズレ」を直す魔法の一行です
+  // GitHub Pagesのサブディレクトリ（/photo-map/）でファイルを正しく探せるようにします
+  base: './', 
+  build: {
+    // ビルド後の出力先を明確にします
+    outDir: 'dist',
+  }
 })
